29ea0eb6d81e1bdca525e04fca9087f152e46fe2
Marlon Mendes Marcondes <mmendes@educa-insights.com.br>
2026-02-25 11:06:49 -0300

    Campaign e Sendwhatsapphandler test

---
+++ Test/Unit/EventListener/CampaignSubscriberTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use MauticPlugin\DialogHSMBundle\EventListener\CampaignSubscriber;
+use PHPUnit\Framework\TestCase;
+
+class CampaignSubscriberTest extends TestCase
+{
+    public function testGetSubscribedEvents(): void
+    {
+        $events = CampaignSubscriber::getSubscribedEvents();
+
+        $this->assertArrayHasKey('mautic.campaign_on_build', $events);
+        $this->assertEquals(['onCampaignBuild', 0], $events['mautic.campaign_on_build']);
+        $this->assertArrayHasKey('mautic.dialoghsm.on_campaign_trigger_action', $events);
+        $this->assertEquals(['onCampaignTriggerAction', 0], $events['mautic.dialoghsm.on_campaign_trigger_action']);
+    }
+}
---
+++ Test/Unit/MessageHandler/SendWhatsAppMessageHandlerTest.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Mautic\CoreBundle\Helper\CoreParametersHelper;
+use MauticPlugin\DialogHSMBundle\Api\DialogHSMApi;
+use MauticPlugin\DialogHSMBundle\Message\SendWhatsAppMessage;
+use MauticPlugin\DialogHSMBundle\MessageHandler\SendWhatsAppMessageHandler;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+class SendWhatsAppMessageHandlerTest extends TestCase
+{
+    private DialogHSMApi&MockObject $mockApi;
+    private EntityManagerInterface&MockObject $mockEntityManager;
+    private LoggerInterface&MockObject $mockLogger;
+    private CoreParametersHelper&MockObject $mockCoreParameters;
+    private Connection&MockObject $mockConnection;
+    private SendWhatsAppMessageHandler $handler;
+
+    protected function setUp(): void
+    {
+        $this->mockApi            = $this->createMock(DialogHSMApi::class);
+        $this->mockEntityManager  = $this->createMock(EntityManagerInterface::class);
+        $this->mockLogger         = $this->createMock(LoggerInterface::class);
+        $this->mockCoreParameters = $this->createMock(CoreParametersHelper::class);
+        $this->mockConnection     = $this->createMock(Connection::class);
+
+        $this->mockEntityManager->method('getConnection')->willReturn($this->mockConnection);
+        $this->mockCoreParameters->method('get')->with('default_timezone')->willReturn('UTC');
+
+        $this->handler = new SendWhatsAppMessageHandler(
+            $this->mockApi,
+            $this->mockEntityManager,
+            $this->mockLogger,
+            $this->mockCoreParameters,
+        );
+    }
+
+    public function testHandleSuccess(): void
+    {
+        $message = new SendWhatsAppMessage(
+            leadId:       1,
+            phone:        '11999999999',
+            apiKey:       'API_KEY',
+            baseUrl:      'https://api.360dialog.com/v1/messages',
+            payloadData:  ['content' => 'nome_template', 'language' => 'pt_BR'],
+            templateName: 'nome_template',
+        );
+
+        $this->mockApi
+            ->expects($this->once())
+            ->method('sendMessage')
+            ->willReturn(['success' => true, 'response' => ['id' => 'abc'], 'error' => null, 'http_status' => 200]);
+
+        $this->mockEntityManager->expects($this->once())->method('persist');
+        $this->mockEntityManager->expects($this->once())->method('flush');
+        $this->mockConnection->expects($this->once())->method('executeStatement');
+
+        ($this->handler)($message);
+    }
+
+    public function testHandleHttpError(): void
+    {
+        $message = new SendWhatsAppMessage(
+            leadId:       1,
+            phone:        '11999999999',
+            apiKey:       'API_KEY',
+            baseUrl:      'https://api.360dialog.com/v1/messages',
+            payloadData:  ['content' => 'nome_template', 'language' => 'pt_BR'],
+            templateName: 'nome_template',
+        );
+
+        $this->mockApi
+            ->expects($this->once())
+            ->method('sendMessage')
+            ->willReturn(['success' => false, 'response' => null, 'error' => 'HTTP 400: Bad Request', 'http_status' => 400]);
+
+        $this->mockEntityManager->expects($this->once())->method('persist');
+        $this->mockEntityManager->expects($this->once())->method('flush');
+        $this->mockConnection->expects($this->once())->method('executeStatement');
+
+        ($this->handler)($message);
+    }
+
+    public function testHandleRequestException(): void
+    {
+        $message = new SendWhatsAppMessage(
+            leadId:       1,
+            phone:        '11999999999',
+            apiKey:       'API_KEY',
+            baseUrl:      'https://api.360dialog.com/v1/messages',
+            payloadData:  ['content' => 'nome_template', 'language' => 'pt_BR'],
+            templateName: 'nome_template',
+        );
+
+        $this->mockApi
+            ->expects($this->once())
+            ->method('sendMessage')
+            ->willReturn(['success' => false, 'response' => null, 'error' => 'Network error', 'http_status' => null]);
+
+        $this->mockEntityManager->expects($this->once())->method('persist');
+        $this->mockEntityManager->expects($this->once())->method('flush');
+        $this->mockConnection->expects($this->once())->method('executeStatement');
+
+        ($this->handler)($message);
+    }
+}
