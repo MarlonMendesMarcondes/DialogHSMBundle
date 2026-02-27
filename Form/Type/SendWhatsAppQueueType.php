6cd4e106f9c79fa3ac57e986852d8c0dac9fa335
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2026-02-27 17:49:26 -0300

    Ajuste redundancia no builder e ajuste de texto

--- Form/Type/SendWhatsAppQueueType.php
+++ Form/Type/SendWhatsAppQueueType.php
@@ -23,14 +23,12 @@ class SendWhatsAppQueueType extends SendWhatsAppType
                     'class'   => 'form-control',
                     'tooltip' => 'dialoghsm.campaign.queue_override.tooltip',
                 ],
-                'choices'     => [
+                'choices'  => [
                     'dialoghsm.campaign.queue_override.choice.queue' => 'queue',
                     'dialoghsm.campaign.queue_override.choice.batch' => 'batch',
                 ],
-                'required'    => false,
-                'placeholder' => 'dialoghsm.campaign.queue_override.placeholder',
-                'empty_data'  => '',
-                'help'        => 'dialoghsm.campaign.queue_override.help',
+                'required' => true,
+                'help'     => 'dialoghsm.campaign.queue_override.help',
             ]
         );
     }
