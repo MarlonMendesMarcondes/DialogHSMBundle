7222af4a2d3d478136993b9946037c5cf52861ce
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2026-03-03 21:20:31 -0300

    teste do sistema de log de envio

---
+++ Test/Unit/Entity/MessageLogRepositoryTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use MauticPlugin\DialogHSMBundle\Entity\MessageLog;
+use MauticPlugin\DialogHSMBundle\Entity\MessageLogRepository;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class MessageLogRepositoryTest extends TestCase
+{
+    private EntityManagerInterface&MockObject $mockEntityManager;
+    private Connection&MockObject $mockConnection;
+    private ClassMetadata&MockObject $mockClassMetadata;
+    private MessageLogRepository&MockObject $repository;
+
+    protected function setUp(): void
+    {
+        $this->mockEntityManager = $this->createMock(EntityManagerInterface::class);
+        $this->mockConnection    = $this->createMock(Connection::class);
+        $this->mockClassMetadata = $this->createMock(ClassMetadata::class);
+
+        $this->mockEntityManager->method('getConnection')->willReturn($this->mockConnection);
+        $this->mockEntityManager->method('getClassMetadata')
+            ->with(MessageLog::class)
+            ->willReturn($this->mockClassMetadata);
+        $this->mockClassMetadata->method('getTableName')->willReturn('dialog_hsm_message_log');
+
+        // Bypass CommonRepository constructor (requires ManagerRegistry);
+        // only mock getEntityManager() so prune() runs its real logic.
+        $this->repository = $this->getMockBuilder(MessageLogRepository::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getEntityManager'])
+            ->getMock();
+
+        $this->repository->method('getEntityManager')->willReturn($this->mockEntityManager);
+    }
+
+    public function testPruneDoesNothingWhenCountBelowLimit(): void
+    {
+        $this->mockConnection
+            ->expects($this->once())
+            ->method('fetchOne')
+            ->with('SELECT COUNT(*) FROM `dialog_hsm_message_log`')
+            ->willReturn('9999');
+
+        $this->mockConnection
+            ->expects($this->never())
+            ->method('executeStatement');
+
+        $this->repository->prune();
+    }
+
+    public function testPruneDoesNothingWhenCountEqualsLimit(): void
+    {
+        $this->mockConnection
+            ->expects($this->once())
+            ->method('fetchOne')
+            ->willReturn('10000');
+
+        $this->mockConnection
+            ->expects($this->never())
+            ->method('executeStatement');
+
+        $this->repository->prune();
+    }
+
+    public function testPruneDeletesOldestWhenOverLimit(): void
+    {
+        $this->mockConnection
+            ->expects($this->once())
+            ->method('fetchOne')
+            ->willReturn('10500');
+
+        $this->mockConnection
+            ->expects($this->once())
+            ->method('executeStatement')
+            ->with('DELETE FROM `dialog_hsm_message_log` ORDER BY date_sent ASC, id ASC LIMIT 500');
+
+        $this->repository->prune();
+    }
+
+    public function testPruneRespectsCustomMaxRecords(): void
+    {
+        $this->mockConnection
+            ->expects($this->once())
+            ->method('fetchOne')
+            ->willReturn('600');
+
+        $this->mockConnection
+            ->expects($this->once())
+            ->method('executeStatement')
+            ->with('DELETE FROM `dialog_hsm_message_log` ORDER BY date_sent ASC, id ASC LIMIT 100');
+
+        $this->repository->prune(maxRecords: 500);
+    }
+
+    public function testPruneDoesNothingWhenTableIsEmpty(): void
+    {
+        $this->mockConnection
+            ->expects($this->once())
+            ->method('fetchOne')
+            ->willReturn('0');
+
+        $this->mockConnection
+            ->expects($this->never())
+            ->method('executeStatement');
+
+        $this->repository->prune();
+    }
+}
